06e83f665e52c7e7c38b9de21202c604b823ab3b
amarildolacerda <amarildo_51@msn.com>
2017-03-01 00:09:47 -0300

    * correção dialec.firebird - nao estava pegando o fields requisitado na url.

--- oData/oData.Dialect.Firebird.pas
+++ oData/oData.Dialect.Firebird.pas
@@ -28,77 +28,77 @@ var
   ARelation: IJsonODataServiceRelation;
   LLevel: Integer;
   child: IODataDecode;
-  sourceKey, targetKey, command: string;
+  sourceKey, targetKey, FJoin: string;
   ATop: Integer;
   FLastFields: String;
+  FFieldsReq:string;
 begin
   oData.Lock;
   try
     inherited createQuery(oData, AFilter, AInLineCount);
     result := 'select ';
     FWhere := '';
-    AResource := GetResource(oData.Resource);
-    FGroupBy := oData.GroupBy;
-    FCollection := AResource.collection;
-    FCollectionFinal := FCollection;
+    AResource := GetResource(oData.Resource); /// busca no metadata os parametros
+    FGroupBy := oData.GroupBy;  /// pega groupby do metadata
+    FCollection := AResource.collection;  /// pega a tabela associada no metadata
+    FCollectionFinal := FCollection;  /// tmp para ultima tabela avalidada
     ATop := oData.Top;
     if ATop = 0 then
-      ATop := AResource.maxpagesize;
+      ATop := AResource.maxpagesize;  /// se nao for requisitado TOP, pegar o constante no metadata
     if AInLineCount = false then
     begin
       if ATop > 0 then
-        result := result + ' first ' + ATop.ToString;
+        result := result + ' first ' + ATop.ToString;   /// $top
       if oData.Skip > 0 then
-        result := result + ' skip ' + oData.Skip.ToString;
-      FFields := oData.Select;
-      if FFields = '' then
-        FFields := AResource.fields;
+        result := result + ' skip ' + oData.Skip.ToString;  /// $skip
+      FFieldsReq := oData.Select;    /// $select
+      FLastFields := AResource.fields;
 
     end
     else
-      FFields := ' count(*) N__Count ';
+      FFieldsReq := ' count(*) N__Count ';   // quando inlinecount=allpages - fazer um count
 
     FLastFields := '';
 
     result := result + ' {%fields} from ' + FCollection + ' as ' +
-      oData.Resource;
+      oData.Resource;                                                   /// monta o select  primeira tabela
 
     // relations
     child := oData;
-    if child.ResourceParams.Count > 0 then
+    if child.ResourceParams.Count > 0 then       /// checa se tem parameteros   ex:   grupos('07')
     begin
-      FKeys := GetWhereFromParams(child, oData.Resource, AResource.keyID);
+      FKeys := GetWhereFromParams(child, oData.Resource, AResource.keyID);  /// gera a where para o parametro
       if FWhere <> '' then
         FWhere := '(' + FWhere + ') and (' + FKeys + ')'
       else
         FWhere := FKeys;
     end;
 
-    if child.hasChild then
-    begin
+    if child.hasChild then  /// tem JOINs ?
+    begin             /// gerar os JOINs
       child := child.child;
       repeat
         // resource pertence ao ultimo resource
 
-        ARelation := AResource.relation(child.Resource);
+        ARelation := AResource.relation(child.Resource);   /// procura o relacionamento "relation" no metadata
         if assigned(ARelation) then
-        begin
-          ARelationResource := GetResource(child.Resource);
+        begin           /// achou um relation
+          ARelationResource := GetResource(child.Resource); /// busca os dados de resource para o relation solicitado (master)
           sourceKey := ARelation.sourceKey;
           targetKey := ARelation.targetKey;
-          command := ARelation.join;
+          FJoin := ARelation.join;
           FCollectionFinal := ARelationResource.collection;
-          FLastFields := ARelationResource.fields;
+          FLastFields := ARelationResource.fields;   /// pega lista de colunas default para o master do relation
 
-          if command <> '' then
-            result := result + ' ' + command
+          if FJoin <> '' then
+            result := result + ' ' + FJoin    /// se tem um join - usa
           else
             result := result + ' join ' + FCollectionFinal + ' as ' +
               child.Resource + ' on (' + oData.Resource + '.' + sourceKey + '='
-              + child.Resource + '.' + targetKey + ')';
+              + child.Resource + '.' + targetKey + ')';   /// quando nao tem JOIN monta um
 
           if child.ResourceParams.Count > 0 then
-          begin
+          begin                                  /// para paramentos passado no relation:   exemplo:   produtos('1')
             FKeys := GetWhereFromParams(child, child.Resource,
               ARelationResource.keyID);
             if FWhere <> '' then
@@ -110,12 +110,12 @@ begin
         end;
         child := child.child;
         if not assigned(child) then
-          break;
-      until child.hasChild;
+          break;                     /// tem mais relation em cascata - se nao tem sai do repeat
+      until child.hasChild;    /// vai para o proximo relation
     end;
 
     if AFilter <> '' then
-    begin
+    begin          /// $filter
       if FWhere <> '' then
         FWhere := '(' + FWhere + ') and ';
       FWhere := FWhere + AFilter;
@@ -124,20 +124,24 @@ begin
     if FWhere <> '' then
       result := result + ' where ' + FWhere;
 
-    if FGroupBy <> '' then
+    if FGroupBy <> '' then               /// url: groupby=xxx
       result := result + ' group by ' + FGroupBy;
 
     if AInLineCount = false then
     begin
       FOrderBy := oData.OrderBy;
       if FOrderBy <> '' then
-        result := result + ' order by ' + FOrderBy;
+        result := result + ' order by ' + FOrderBy;   /// url:   $orderby
     end;
 
     if (FLastFields <> '') and (not AInLineCount) then
-      FFields := FLastFields;
+      FFields := FLastFields;   /// usa o ultimo
+
+    if FFieldsReq<>'' then
+      FFields := FFieldsReq;   // manter o que foi requisitado na url - nao usar o ultimo
+
     if FFields = '' then
-      FFields := '*';
+      FFields := '*';      /// se nao foi indicado nenhum field - usa *
     result := stringReplace(result, '{%fields}', FFields, []);
   finally
     oData.Unlock;
---
+++ oData/oData.Parse_old.pas
@@ -0,0 +1,425 @@
+unit oData.Parse;
+
+interface
+
+uses System.Classes, System.SysUtils, System.Generics.collections,
+  oData.Engine, oData.Interf;
+
+type
+
+  TTokenKind = (ptNone, ptIdentifier, ptNull, ptSpace, ptComma, ptSlash,
+    ptOData, ptOpen, ptClose, ptParams, ptParamsAnd, ptEqual, ptFilter,
+    ptSelect, ptOrderBy, ptTop, ptSkip, ptSkipToken, ptExpand, ptInLineCount,
+    ptGroupBy, ptOperNe { not equal } , ptOperLt { less than } ,
+    ptOperGe { greater or equal } , ptOperGt { greater than } ,
+    ptOperAnd { and } , ptOperOr { or } , ptOperNot { not } ,
+    ptOperAdd { add } , ptOperSub { subtract } , ptOperMul { multiply } ,
+    ptOperDiv { divide } , ptOperMod { find remainder } ,
+    ptFuncFloor { floor } , ptFuncCeiling { ceiling } ,
+    ptFuncLength { length } , ptFuncTrim { trim } , ptFuncToUpper { toupper } ,
+    ptFuncSubstringOf { substringof } , ptFuncToLower { tolower } ,
+    ptFuncEndsWith { endswith } , ptFuncStartsWith { startswith } ,
+    ptFuncYear { year } , ptFuncMonth { month } , ptFuncDay { day } ,
+    ptFuncHour { hour } , ptFuncMinute { minute } , ptFuncSecond { second } ,
+    ptOrderByDesc { desc } );
+
+  {
+    - add,sub,mul,div,mod -> can use only in $filter   not  $select
+
+  }
+
+  TODataParse = class(TInterfacedObject, IODataParse)
+  private
+    FUrl: string;
+    token: string;
+    FParseCol: integer;
+    FLevel: integer;
+    FOData: IODataDecode;
+    FCurrentOData: IODataDecode;
+
+    function isToken(tken: string): boolean;
+    procedure NextToken(AClear: boolean = false; AUntil: TTokenKind = ptNone);
+    function TestNextToken: string;
+    function IsNullToken(AChr: Char): boolean;
+    function isTokenType(tk: TTokenKind): boolean;
+    function IsNull: boolean;
+    function GetToken: string;
+    procedure ParseCollectionParams;
+    procedure ParseCollections;
+    procedure ParseParams;
+    procedure SetOData(const Value: TODataDecode);
+    function GetOData: IODataDecode;
+  public
+    constructor create;
+    destructor destroy; override;
+    property oData: IODataDecode read GetOData; // write SetOData;
+    function isTokenIsJunk(const tk: TTokenKind): boolean;
+    procedure whileIn(AArry: Array of TTokenKind);
+    procedure FindToken(tk: TTokenKind);
+  public
+    procedure Parse(URL: string); virtual;
+    procedure DoCollectionInsert(ACurrent: IODataDecode); virtual;
+    function Select: string; virtual;
+  end;
+
+implementation
+
+{ TODataParse }
+
+var
+  FTokenKindArray: TDictionary<string, TTokenKind>;
+
+function TODataParse.isToken(tken: string): boolean;
+begin
+  result := FTokenKindArray.ContainsKey(tken);
+end;
+
+function TODataParse.isTokenIsJunk(const tk: TTokenKind): boolean;
+begin
+  result := tk in [ptOData];
+end;
+
+function TODataParse.isTokenType(tk: TTokenKind): boolean;
+var
+  t: TTokenKind;
+begin
+  result := false;
+  if FTokenKindArray.TryGetValue(token, t) then
+    result := t = tk
+  else
+    result := (token <> '') and (tk = ptIdentifier);
+end;
+
+constructor TODataParse.create;
+begin
+  inherited;
+  FOData := TODataDecode.create(self);
+end;
+
+destructor TODataParse.destroy;
+begin
+  FOData := nil;
+  inherited;
+end;
+
+procedure TODataParse.DoCollectionInsert(ACurrent: IODataDecode);
+begin
+
+end;
+
+procedure TODataParse.FindToken(tk: TTokenKind);
+var
+  sToken: string;
+  t: TTokenKind;
+begin
+  sToken := '';
+  repeat
+    sToken := sToken + FUrl[FParseCol];
+    inc(FParseCol);
+    if isToken(FUrl[FParseCol]) then
+      if FTokenKindArray.TryGetValue(sToken, t) then
+        if tk = t then
+          break;
+    if IsNull then
+      break;
+  until false;
+end;
+
+function TODataParse.GetOData: IODataDecode;
+begin
+  result := FOData;
+end;
+
+function TODataParse.GetToken: string;
+begin
+  result := token;
+  token := '';
+end;
+
+function TODataParse.IsNull: boolean;
+begin
+  result := IsNullToken(FUrl[FParseCol]);
+end;
+
+function TODataParse.IsNullToken(AChr: Char): boolean;
+begin
+  result := AChr = chr(0);
+end;
+
+procedure TODataParse.NextToken(AClear: boolean = false;
+  AUntil: TTokenKind = ptNone);
+var
+  tk, tkj: TTokenKind;
+  tmp: string;
+  b: boolean;
+begin
+  if AClear then
+    token := '';
+  repeat
+    if IsNullToken(FUrl[FParseCol]) then
+      exit;
+
+    if (AUntil <> ptNone) then
+      if FTokenKindArray.TryGetValue(FUrl[FParseCol], tkj) then
+      begin
+        if tkj = AUntil then
+          break;
+      end;
+
+    b := true;
+    if (AUntil <> ptNone) then
+    begin
+      FTokenKindArray.TryGetValue(tmp, tkj);
+      if tkj = AUntil then
+        break
+      else
+      begin
+        if tkj <> ptNone then
+          tmp := '';
+        b := false;
+        // continue;
+      end;
+    end;
+
+    if (FUrl[FParseCol] <> ' ') or (AUntil <> ptNone) then
+    begin
+      token := token + FUrl[FParseCol];
+      tmp := tmp + FUrl[FParseCol];
+    end;
+    inc(FParseCol);
+
+    if (AUntil = ptNone) then
+      if FTokenKindArray.TryGetValue(FUrl[FParseCol], tkj) then
+        if tkj in [ptSpace, ptOpen, ptClose, ptComma, ptEqual, ptParamsAnd,
+          ptParams] then
+          break;
+
+  until isToken(token) and b;
+
+  if not FTokenKindArray.TryGetValue(token, tk) then
+  begin
+    exit;
+  end;
+
+  if tk = ptSlash then
+  begin
+    if FTokenKindArray.TryGetValue(TestNextToken, tkj) then
+    begin
+      if isTokenIsJunk(tkj) then
+      begin
+        NextToken;
+      end;
+    end;
+  end;
+
+  if tk = ptNull then
+    exit;
+
+  if isTokenIsJunk(tk) then
+  begin
+    token := '';
+    NextToken;
+  end;
+
+end;
+
+procedure TODataParse.ParseCollections;
+begin
+  GetToken; // clear;
+  NextToken;
+  FCurrentOData := FOData.GetLevel(FLevel, true);
+  FCurrentOData.Resource := GetToken;
+  if FCurrentOData.Resource = '' then
+    exit;
+
+  FCurrentOData.ResourceParams.clear;
+  if not IsNull then
+    ParseCollectionParams;
+  DoCollectionInsert(FCurrentOData);
+  if IsNull then
+    exit;
+  if isTokenType(ptParams) then
+  begin
+    ParseParams;
+    exit;
+  end;
+  inc(FLevel);
+  ParseCollections;
+end;
+
+procedure TODataParse.ParseParams;
+var
+  s, k: string;
+  tk: TTokenKind;
+begin
+  NextToken(true);
+  s := GetToken;
+  NextToken(true);
+  if isTokenType(ptEqual) then
+  begin
+    NextToken(true, ptParamsAnd);
+    k := GetToken;
+    if FTokenKindArray.TryGetValue(s, tk) then
+    begin
+      case tk of
+        ptGroupBy:
+          oData.GroupBy := k;
+        ptSelect:
+          oData.Select := k;
+        ptFilter:
+          oData.Filter := k;
+        ptOrderBy:
+          oData.OrderBy := k;
+        ptTop:
+          oData.top := strtoIntDef(k, 0);
+        ptSkip:
+          oData.Skip := strtoIntDef(k, 0);
+        ptSkipToken:
+          oData.SkipToken := k;
+        ptInLineCount:
+          oData.InLineCount := k;
+        ptExpand:
+          oData.Expand := k;
+      end;
+    end;
+
+    // FOData.ResourceParams.AddPair(s, k); // QueryParams.Add(s, k);
+  end;
+  NextToken(true);
+  if IsNull then
+    exit;
+  if isTokenType(ptParamsAnd) then
+  begin
+    ParseParams;
+  end;
+end;
+
+function TODataParse.Select: string;
+begin
+  result := oData.Select;
+end;
+
+procedure TODataParse.SetOData(const Value: TODataDecode);
+begin
+  FOData := Value;
+end;
+
+procedure TODataParse.Parse(URL: string);
+begin
+  FLevel := 0;
+  FUrl := URL + chr(0);
+  FParseCol := 1;
+  ParseCollections;
+  if isTokenType(ptParams) then
+    ParseParams;
+end;
+
+procedure TODataParse.ParseCollectionParams;
+var
+  nome,s: String;
+  i: integer;
+  oper:string;
+begin
+  whileIn([ptSpace]);
+  NextToken;
+  i := 0;
+  if isTokenType(ptOpen) then
+  begin
+    repeat
+      nome:='';
+      if IsNull then
+        break;
+      inc(i);
+      GetToken; // clear;
+      whileIn([ptSpace]);
+      NextToken;
+      nome := '__P'+IntToStr(i);
+      if isTokenType(ptClose) then
+        break;
+      s := GetToken;
+      if TestNextToken='=' then
+      begin
+        nome := s;
+        NextToken;
+        oper := GetToken; // clear;
+        NextToken;
+      end;
+      s := GetToken;
+      FCurrentOData.ResourceParams.AddPair(nome, s);
+      whileIn([ptSpace, ptComma]);
+      if isTokenType(ptComma) then
+        continue;
+    until isTokenType(ptClose);
+    if isTokenType(ptClose) then
+      NextToken(true);
+  end;
+end;
+
+function TODataParse.TestNextToken: string;
+var
+  i: integer;
+begin
+  result := token;
+  i := FParseCol;
+  repeat
+    if IsNullToken(FUrl[i]) then
+      break;
+    result := result + FUrl[i];
+    inc(i);
+  until isToken(result);
+end;
+
+procedure TODataParse.whileIn(AArry: array of TTokenKind);
+var
+  t: string;
+  tk: TTokenKind;
+  tmp: TTokenKind;
+  b: boolean;
+begin
+  repeat
+    t := t + FUrl[FParseCol];
+    b := false;
+    if FTokenKindArray.TryGetValue(t, tk) then
+      for tmp in AArry do
+      begin
+        if (tk = tmp) then
+        begin
+          b := true;
+          break;
+        end;
+      end;
+    if not b then
+      exit;
+    inc(FParseCol);
+  until isToken(t);
+end;
+
+initialization
+
+FTokenKindArray := TDictionary<string, TTokenKind>.create;
+FTokenKindArray.Add(',', ptComma);
+FTokenKindArray.Add(chr(0), ptNull);
+FTokenKindArray.Add('/', ptSlash);
+FTokenKindArray.Add('/OData/', ptOData);
+FTokenKindArray.Add('OData.svc/', ptOData);
+FTokenKindArray.Add('(', ptOpen);
+FTokenKindArray.Add(')', ptClose);
+FTokenKindArray.Add(' ', ptSpace);
+FTokenKindArray.Add('?', ptParams);
+FTokenKindArray.Add('&', ptParamsAnd);
+FTokenKindArray.Add('=', ptEqual);
+FTokenKindArray.Add('$filter', ptFilter);
+FTokenKindArray.Add('$select', ptSelect);
+FTokenKindArray.Add('$orderby', ptOrderBy);
+FTokenKindArray.Add('$top', ptTop);
+FTokenKindArray.Add('$skip', ptSkip);
+FTokenKindArray.Add('$skiptoken', ptSkipToken);
+FTokenKindArray.Add('$inlinecount', ptInLineCount);
+FTokenKindArray.Add('$expand', ptExpand);
+FTokenKindArray.Add('groupby', ptGroupBy);
+
+finalization
+
+FTokenKindArray.free;
+
+end.
